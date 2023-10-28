9d75776b7a32923b7306e28c2086e1a8f345ffd2
LVMdS <leo92vinicius@gmail.com>
2023-10-28 20:08:17 -0300

    Adicionado estilo ao menu, bem basico, feito menu hambueguer para telas pequenas

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const menu = document.querySelector('.menu');
+    const searchInput = document.getElementById('search-input');
+    const searchButton = document.getElementById('search-button');
+
+    menuIcon.addEventListener('click', function () {
+        menu.classList.toggle('show');
+    });
+
+    searchButton.addEventListener('click', function () {
+        const searchTerm = searchInput.value;
+        if (searchTerm) {
+
+            window.location.href = `search.html?query=${encodeURIComponent(searchTerm)}`;
+        }
+    });
+
+    searchInput.addEventListener('keyup', function (event) {
+        if (event.key === 'Enter') {
+            searchButton.click();
+        }
+    });
+});
+
 const slider = document.querySelector('.slider');
 const slides = document.querySelector('.slides');
 const slideWidth = slider.clientWidth; 
